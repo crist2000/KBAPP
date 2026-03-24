e563c87fb0647a20c612a611a9cad3c61df132cf
mark.jurkin@gmail.com <mark.jurkin@gmail.com>
2026-03-24 15:24:39 +0200

    Implemented aility to use commas in data fields

--- gui/kbdata.py
+++ gui/kbdata.py
@@ -7,7 +7,7 @@ else:
 
     def formatStr(lst: list) -> str:
         str_res = "".join(lst)
-        return str_res.replace("'", "").replace(")", "")
+        return str_res.replace("'", "").replace(")", "").replace("None", "")
 
     class kbData:
         __clt = []
@@ -25,26 +25,26 @@ else:
 
         def __init__(self, sql_raw):
             self.__clearKBdata()
+
             try:
                 for row in sql_raw:
-                    a, b, c, d, e, f = str(row).split(", ")
-                    self.__clt.append(b + "\n")
-                    self.__prd.append(c + "\n")
-                    self.__err.append(d + "\n")
-                    self.__cse.append(e + "\n")
-                    self.__fix.append(f + "\n")
+                    resp = row[1:] #filter out row ID
+                    self.__clt.append(str(resp[ColIdx.clt.value]) + "\n")
+                    self.__prd.append(str(resp[ColIdx.prd.value]) + "\n")
+                    self.__err.append(str(resp[ColIdx.err.value]) + "\n")
+                    self.__cse.append(str(resp[ColIdx.cse.value]) + "\n")
+                    self.__fix.append(str(resp[ColIdx.fix.value]) + "\n")
 
                 self.client = formatStr(self.__clt)
                 self.prod = formatStr(self.__prd)
                 self.error = formatStr(self.__err)
                 self.cause = formatStr(self.__cse)
                 self.fix = formatStr(self.__fix)
-
             except:
-                self.client = ""
-                self.prod = ""
-                self.error = ""
-                self.cause = ""
-                self.fix = ""
+                 self.client = ""
+                 self.prod = ""
+                 self.error = ""
+                 self.cause = ""
+                 self.fix = ""
 
-                print(f"{APP}SQL raw data was not recognized. Data parsing failed.")
+                 print(f"{APP}SQL raw data was not recognized. Data parsing failed.")
